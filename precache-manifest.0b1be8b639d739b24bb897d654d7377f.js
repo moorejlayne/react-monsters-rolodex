e1a63a5b6fce588407f0db6ef24e2bd3215f7a31
Jacob L Moore <jacob.moore@daugherty.com>
2020-01-12 16:02:00 -0600

    Updates

---
+++ precache-manifest.0b1be8b639d739b24bb897d654d7377f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce812a5838285e641d89f1070f1c043c",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a059f6029ec91eb2e5e0",
+    "url": "/react-monsters-rolodex/static/css/main.e1ccd115.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/react-monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a059f6029ec91eb2e5e0",
+    "url": "/react-monsters-rolodex/static/js/main.b4f2e142.chunk.js"
+  },
+  {
+    "revision": "a17f75d38f92581ed444",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.7e4f218e.js"
+  }
+]);
